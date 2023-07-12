3ffbebd3fecb80e5315bf7b03276b21e3a027334
Paul Buscano <pbuscano@propertyvista.com>
2023-07-12 23:28:48 +0800

    [FIXED] quote error

--- components/home/OurStory.tsx
+++ components/home/OurStory.tsx
@@ -6,7 +6,7 @@ function OurStory() {
       <h3 className="text-3xl mb-4">August, 2016</h3>
       <p className="max-w-screen-md text-center mb-10">
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
