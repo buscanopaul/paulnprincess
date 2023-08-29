783d41f6ed71d8f9091d5e3374b2a5386e0fcef7
Paul Buscano <pbuscano@propertyvista.com>
2023-08-29 14:02:09 +0800

    [FIXED] og description

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
       <head>
         <meta
           property="og:image"
-          content="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+          content="https://paulnprincess.vercel.app/images/og.jpg"
         />
         <meta property="og:title" content="#PrincessSavedTheBestPaulast" />
         <meta property="og:description" content="Save The Date" />
